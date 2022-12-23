c67615ab3d81a92bceafb83ccd0e8d0a6d904f4f
Alhaimouni <m.alhaumouni@gmail.com>
2022-12-23 19:19:00 +0200

    add home page style

--- src/components/HeaderBackground.js
+++ src/components/HeaderBackground.js
@@ -5,7 +5,7 @@ function HeaderBackground() {
   return (
     <div className='header-background'>
       <h1>COVID <span>19</span></h1>
-      <p>Covid19 Statistics</p>
+      <p>Covid-19 Statistics</p>
       <p> A Website to provide you with all the updates on Covid-19 statistics around the world </p>
     </div>
   )
